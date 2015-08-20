c9320cf611c677a2c1e451725234b94bd8ad3972
신경수 <kyungsoo.shin@gmail.com>
2015-08-20 16:56:41 +0900

    Autofac ioc container 모듈 기능 추가

---
+++ BizOneShot.Light.Web/App_Start/AutofacModule.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Reflection;
+
+using Autofac;
+using Autofac.Integration.Mvc;
+
+using BizOneShot.Light.Dao.Infrastructure;
+using BizOneShot.Light.Dao.Repositories;
+using BizOneShot.Light.Services;
+
+namespace BizOneShot.Light.Web.App_Start
+{
+    public static class AutofacModule
+    {
+        public static void Run()
+        {
+
+        }
+
+        private static void SetAutofacContainer()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerRequest();
+            builder.RegisterType<DbFactory>().As<IDbFactory>().InstancePerRequest();
+
+            // Repositories
+            //builder.RegisterAssemblyTypes(typeof(ScCompInfoRepository).Assembly)
+            //    .Where(t => t.Name.EndsWith("Repository"))
+            //    .AsImplementedInterfaces().InstancePerRequest();
+
+            builder.RegisterAssemblyTypes(Assembly.Load("BizOneShot.Light.Dao.Repositories"))
+                .Where(t => t.Name.EndsWith("Repository"))
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope();
+
+            // Services
+            //builder.RegisterAssemblyTypes(typeof(ScCompInfoService).Assembly)
+            //   .Where(t => t.Name.EndsWith("Service"))
+            //   .AsImplementedInterfaces().InstancePerRequest();
+
+            builder.RegisterAssemblyTypes(Assembly.Load("BizOneShot.Light.Services"))
+                .Where(t => t.Name.EndsWith("Service"))
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope();
+        }
+    }
+}
