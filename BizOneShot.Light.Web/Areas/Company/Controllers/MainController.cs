9ef61f2fa3ac553fc714b555de71cb1f927b38fd
기인 신 <simpsonz@netsgo.com>
2015-08-28 09:31:49 +0900

    Areas Company 추가

---
+++ BizOneShot.Light.Web/Areas/Company/Controllers/MainController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BizOneShot.Light.Web.Areas.Company.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: Company/Main
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
