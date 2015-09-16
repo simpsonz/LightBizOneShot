a11718d9c58403e09064e7b45de5140de79068ba
신경수 <kyungsoo.shin@gmail.com>
2015-09-16 16:15:22 +0900

    ScCompMappingService 생성

---
+++ BizOneShot.Light.Services/ScCompMappingService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BizOneShot.Light.Dao.Infrastructure;
+using BizOneShot.Light.Dao.Repositories;
+using BizOneShot.Light.Models.WebModels;
+
+namespace BizOneShot.Light.Services
+{
+    public interface IScCompMappingService : IBaseService
+    {
+        //Task<IList<ScCompMapping>> GetCompMappingsAsync(string mentorId = null, string status = null);
+    }
+
+
+    public class ScCompMappingService : IScCompMappingService
+    {
+        private readonly IScCompMappingRepository scCompMappingRepository;
+        private readonly IUnitOfWork unitOfWork;
+
+        public ScCompMappingService(IScCompMappingRepository scCompMappingRepository, IUnitOfWork unitOfWork)
+        {
+            this.scCompMappingRepository = scCompMappingRepository;
+            this.unitOfWork = unitOfWork;
+        }
+
+
+        //public async Task<IList<ScCompMapping>> GetCompMappingsAsync(string mentorId = null, string status = null)
+        //{
+        //}
+
+        #region SaveContext
+        public void SaveDbContext()
+        {
+            unitOfWork.Commit();
+        }
+
+        public async Task<int> SaveDbContextAsync()
+        {
+            return await unitOfWork.CommitAsync();
+        }
+        #endregion
+    }
+}
