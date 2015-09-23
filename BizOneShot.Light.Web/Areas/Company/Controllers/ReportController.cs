6b0f7ea87ec218c8ad3435b77cd37f4eec936f8a
기인 신 <simpsonz@netsgo.com>
2015-09-23 15:04:55 +0900

    기초역량문진표 개발 시작

---
+++ BizOneShot.Light.Web/Areas/Company/Controllers/ReportController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BizOneShot.Light.Web.ComLib;
+
+namespace BizOneShot.Light.Web.Areas.Company.Controllers
+{
+    public class ReportController : Controller
+    {
+        // GET: Company/Report
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult BasicSurvey()
+        {
+            ViewBag.LeftMenu = Global.Report;
+            return View();
+        }
+
+        public ActionResult Summary01()
+        {
+            ViewBag.LeftMenu = Global.Report;
+            return View();
+        }
+
+        public ActionResult FinanceMng()
+        {
+            ViewBag.LeftMenu = Global.Report;
+            return View();
+        }
+
+
+
+        
+    }
+}
