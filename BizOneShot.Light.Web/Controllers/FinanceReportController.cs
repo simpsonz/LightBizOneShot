50ca54165ef6abdfe1db30cd8f055dd589975838
기인 신 <simpsonz@netsgo.com>
2015-10-12 13:44:27 +0900

    재무보고서 작성 시작

---
+++ BizOneShot.Light.Web/Controllers/FinanceReportController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BizOneShot.Light.Models.ViewModels;
+using BizOneShot.Light.Web.ComLib;
+
+namespace BizOneShot.Light.Web.Controllers
+{
+    public class FinanceReportController : Controller
+    {
+        [UserAuthorize(Order = 1)]
+        [MenuAuthorize(Roles = UserType.Company | UserType.Expert | UserType.SysManager, Order = 2)]
+        // GET: FinanceReopert
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult FinanceMng()
+        {
+            ViewBag.LeftMenu = Global.Report;
+
+            return View();
+        }
+    }
+}
