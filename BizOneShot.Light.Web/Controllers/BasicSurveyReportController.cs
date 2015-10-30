03f76b7758faafe9b50fbcc30d60bf06d0707567
신경수 <kyungsoo.shin@gmail.com>
2015-10-30 19:04:08 +0900

    에러수정

--- BizOneShot.Light.Web/Controllers/BasicSurveyReportController.cs
+++ BizOneShot.Light.Web/Controllers/BasicSurveyReportController.cs
@@ -425,7 +425,7 @@ namespace BizOneShot.Light.Web.Controllers
 
 
                     //종합점수 조회하여 분류별로 딕셔너리 저장
-                    var point = await reportUtil.GetCompanyTotalPoint(quesMasters.QuestionSn);
+                    //var point = await reportUtil.GetCompanyTotalPoint(quesMasters.QuestionSn);
 
             //        if (point >= 0 && point <= 50)
             //            dicStartUp.Add(compMapping.CompSn, quesMasters.QuestionSn);
@@ -544,7 +544,7 @@ namespace BizOneShot.Light.Web.Controllers
 
 
                     //종합점수 조회하여 분류별로 딕셔너리 저장
-                    var point = await reportUtil.GetCompanyTotalPoint(quesMasters.QuestionSn);
+                    //var point = await reportUtil.GetCompanyTotalPoint(quesMasters.QuestionSn);
 
             //        if (point >= 0 && point <= 50)
             //            dicStartUp.Add(compMapping.CompSn, quesMasters.QuestionSn);
@@ -636,7 +636,7 @@ namespace BizOneShot.Light.Web.Controllers
         {
             ViewBag.LeftMenu = Global.CapabilityReport;
 
-            ReportUtil reportUtil = new ReportUtil(quesResult1Service, quesResult2Service, quesMasterService);
+            ReportUtil reportUtil = new ReportUtil(scBizWorkService, quesResult1Service, quesResult2Service, quesMasterService);
 
             OrgProductivityViewModel viewModel = new OrgProductivityViewModel();
             viewModel.CheckList = new List<CheckListViewModel>();
@@ -753,7 +753,7 @@ namespace BizOneShot.Light.Web.Controllers
         {
             ViewBag.LeftMenu = Global.CapabilityReport;
 
-            ReportUtil reportUtil = new ReportUtil(quesResult1Service, quesResult2Service, quesMasterService);
+            ReportUtil reportUtil = new ReportUtil(scBizWorkService, quesResult1Service, quesResult2Service, quesMasterService);
 
             var viewModel = new OrgDividedViewModel();
 
